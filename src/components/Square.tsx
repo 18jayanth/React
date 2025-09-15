006b1ee8772a6fa6c7e7602b58cb6b2a160604fb
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-15 10:17:21 +0530

    Delete src directory

--- src/components/Square.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-type MyComponentProps = {
-  value: string;
-  onSquareClick: () => void;
-};
-export default function Square({value,onSquareClick}:MyComponentProps) {
-    
-  return <button onClick={onSquareClick} className="square">{value}</button>;
-}
