1481c08d4c3d63ab99e2a2addc1db49d78b38839
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-23 18:50:20 +0530

    Add files via upload

---
+++ React Hooks/useContext/Sigma Web Developement/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { counterContext } from "./components/context/context";
+function App() {
+  const [count, setCount] = useState(0);
+return (
+    <>
+    <counterContext.Provider value={{count,setCount}}>
+        <Navbar />
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          </div>
+        </counterContext.Provider>
+    </>
+  );
+}
+
+export default App;
