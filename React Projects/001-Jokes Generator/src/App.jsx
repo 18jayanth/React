aef59b887b7085dc82fb20e49f21baa6a1e2e075
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-09-28 15:38:00 +0530

    Add files via upload

---
+++ React Projects/001-Jokes Generator/src/App.jsx
@@ -0,0 +1,11 @@
+import JokesGenerator from "./components/JokesGenerator"
+import './App.css'
+export default function App()
+{
+  return(
+    <>
+    <h1>Jokes Generator Using React and Jokes API</h1>
+    <JokesGenerator/>
+    </>
+  )
+}
